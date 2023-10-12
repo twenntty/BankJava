a10428d7a0326a51ebb91d0f1d812f5284e120b4
Mark Bilous <belousme601@gmail.com>
2023-10-12 15:06:33 +0300

    Created test classes where we simulate different scenarios to test exception handling.

--- src/main/java/org/example/BankTest.java
+++ src/main/java/org/example/BankTest.java
@@ -19,4 +19,26 @@ class AccountNotFoundException extends Exception {
 }
 
 public class BankTest {
+    public static void main(String[] args) {
+        Bank bank = new Bank();
+
+        try {
+            BankAccount account1 = bank.createAccount("Alice", 1000);
+            BankAccount account2 = bank.createAccount("Bob", 500);
+
+            System.out.println(account1.getAccountSummary());
+            System.out.println(account2.getAccountSummary());
+
+            bank.transferMoney(1, 2, 300);
+
+            System.out.println(account1.getAccountSummary());
+            System.out.println(account2.getAccountSummary());
+
+            bank.transferMoney(1, 2, -100);
+        } catch (NegativeAmountException | InsufficientFundsException e) {
+            System.out.println("Exception: " + e.getMessage());
+        } catch (javax.security.auth.login.AccountNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
